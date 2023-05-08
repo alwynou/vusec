50d6df5cf8ae2f295e61476e2ee441d8cd3b7554
alwynou <alwynou@163.com>
2023-05-08 13:40:36 +0800

    chore: setup

---
+++ build.config.ts
@@ -0,0 +1,12 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    './src/index',
+  ],
+  outDir: 'dist',
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+  },
+})
